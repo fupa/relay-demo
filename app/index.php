2a834546cde4213596017b00e7d926da4cf2340b
Sepp Stadler <j.stadler@fupa.net>
2025-02-14 15:33:12 +0100

    fix: invalid comment

--- app/index.php
+++ app/index.php
@@ -8,7 +8,7 @@ const CACHE_DURATION_IN_SECONDS = 25;
 const CACHE_KEY = 'test-key';
 $relay = new Relay();
 
-$relay->setOption(Relay::OPT_THROW_ON_ERROR, true); // seconds
+$relay->setOption(Relay::OPT_THROW_ON_ERROR, true);
 $relay->setOption(Relay::OPT_MAX_RETRIES, 3);
 $relay->setOption(Relay::OPT_BACKOFF_BASE, 10); // ms
 $relay->setOption(Relay::OPT_BACKOFF_CAP, 25); // ms
